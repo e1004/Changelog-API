f36a30b1f3b8de7128fa261f15e7d5ca1157586e
elisabeth <elisabeth.r2ni@gmail.com>
2024-12-31 19:09:27 +0200

    Delete redundant test fixture

--- test/test_repository_create.py
+++ test/test_repository_create.py
@@ -1,6 +1,3 @@
-import os
-import sqlite3
-from contextlib import closing
 from datetime import UTC, datetime
 from uuid import UUID
 
@@ -17,15 +14,6 @@ def project_1():
     project_repo.delete_project(p.id)
 
 
-@pytest.fixture
-def clean_db():
-    with closing(
-        sqlite3.connect(os.environ["PROJECT_DATABASE_PATH"], uri=True, autocommit=True)
-    ):
-        yield
-
-
-@pytest.mark.usefixtures("clean_db")
 def test_it_creates_version(project_1: Project):
     # given
     version_number = "2.3.5"
