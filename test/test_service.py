0db15b1dc2227062babb5af83bb36acc28410e6c
elisabeth <elisabeth.r2ni@gmail.com>
2025-01-01 18:15:46 +0200

    Add service test

--- test/test_service.py
+++ test/test_service.py
@@ -1,5 +1,9 @@
-import pytest
+from uuid import uuid4
 
+import pytest
+from pytest_mock import MockerFixture
+
+from e1004.changelog_api import repository, service
 from e1004.changelog_api.error import VersionNumberInvalidError
 from e1004.changelog_api.service import validate_version_number
 
@@ -21,3 +25,17 @@ def test_it_returns_valid_version_number():
     result = validate_version_number(number)
     # then
     assert result == number
+
+
+def test_it_creates_version_calls_repository(mocker: MockerFixture):
+    # given
+    create_version = mocker.patch.object(repository, "create_version")
+    number = "1.2.3"
+    project_id = uuid4()
+
+    # when
+    result = service.create_version(number, project_id)
+
+    # then
+    create_version.assert_called_once_with(number, project_id)
+    assert result == create_version.return_value
