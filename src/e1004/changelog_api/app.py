7c58b4aec54f963c295bf15ab468a407d077f8f5
elisabeth <elisabeth.r2ni@gmail.com>
2025-01-09 20:17:03 +0200

    Add public index HTML for versions

--- src/e1004/changelog_api/app.py
+++ src/e1004/changelog_api/app.py
@@ -5,11 +5,13 @@ from flask.json.provider import DefaultJSONProvider
 from realerikrani.project import register_project
 
 from e1004.changelog_api.blueprint import version
+from e1004.changelog_api.ui import ui
 
 
 def create() -> Flask:
     app = register_project(Flask("changelog_api"))
     app.register_blueprint(version, url_prefix="/versions")
+    app.register_blueprint(ui)
     app.json.default = (  # type: ignore[attr-defined]
         lambda obj: obj.isoformat()
         if isinstance(obj, datetime | date)
---
+++ src/e1004/changelog_api/ui.py
@@ -0,0 +1,25 @@
+from uuid import UUID
+
+from flask import Blueprint, render_template, request
+
+from e1004.changelog_api import service
+
+ui = Blueprint("ui_controller", __name__, template_folder="templates")
+
+
+@ui.route("/<uuid:project_id>", methods=["GET", "POST"])
+def index(project_id: UUID):  # noqa: ANN201
+    token = None
+    if request.method == "POST":
+        if "load_next" in request.form:
+            token = request.form.get("next", None)
+        elif "load_previous" in request.form:
+            token = request.form.get("previous", None)
+    versions_page = service.read_versions(project_id, 5, token)
+
+    return render_template(
+        "index.html",
+        versions=versions_page.versions,
+        previous_token=versions_page.prev_token,
+        next_token=versions_page.next_token,
+    )
